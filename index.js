dd56406030fec92afc71449858eb9e422be62947
Sebas Merino <sebas.mernor@gmail.com>
2023-03-18 14:31:34 -0300

    actualizacion

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-for (let i = 0; i < 12; i++) {
+for (let i = 1; i < 12; i++) {
     alert("lectura n°" + i)
+    let voltaje = prompt("Ingrese voltaje");
+    alert("El voltaje es " + voltaje + " volts");
+        if (voltaje === null){
+            valorIncorrecto;
+             alert ("Valor no introducido.")
+            } else {
+                 console.log("Falta valor")}
+    let amperaje = prompt("Ingrese corriente");
+    alert("La corriente es " + amperaje + " ampers");
+    if (amperaje === null){
+        alert ("Valor no introducido.")
+       } else {
+            console.log("Falta valor")}
+    let resistencia = prompt("Ingrese resistencia");
+    alert("La resistencia es " + resistencia + " ohms");
+    if (resistencia === null){
+        alert ("Valor no introducido.")
+       } else {
+            console.log("Falta valor")}
+} 
 
-let voltaje = prompt("Ingrese voltaje");
-    alert("El voltaje es " + voltaje);
-    
-
-let amperaje = prompt("Ingrese corriente");
-    alert("La corriente es " + amperaje);
-    
-let resistencia = prompt("Ingrese resistencia");
-    alert("La resistencia es " + resistencia);
-
-if (voltaje === null ) {
-    
-}
-
-}  
-
-
-
-const lectura = alert('');
+function lectura () {
 console.log("Voltaje: "+ voltaje + " volts");
 console.log("Corriente: "+ amperaje + " amperes");
 console.log("Resistencia: "+ resistencia + " ohms");
+}
 
+lectura();
